de9c38049be97ae07e43391699ffee052fe19f6b
Allen <tuk04440@temple.edu>
2024-02-08 11:31:04 -0500

    Implemented TextSizeAdapter and assigned as adapter for spinner.

--- app/src/main/java/edu/temple/inclassuiacvitivity/TextSizeAdapter.kt
+++ app/src/main/java/edu/temple/inclassuiacvitivity/TextSizeAdapter.kt
@@ -1,25 +1,34 @@
 package edu.temple.inclassuiacvitivity
 
+import android.content.Context
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
+import android.widget.Spinner
+import android.widget.TextView
 
-class TextSizeAdapter : BaseAdapter() {
+class TextSizeAdapter(_context: Context, _numberArray: Array<Int>) : BaseAdapter() {
 
+    private val context = _context
+    private val numberArray = _numberArray
     override fun getCount(): Int {
-        TODO("Not yet implemented")
+        return numberArray.size
     }
 
     override fun getItem(position: Int): Any {
-        TODO("Not yet implemented")
+        return numberArray[position]
     }
 
     override fun getItemId(position: Int): Long {
-        TODO("Not yet implemented")
+        return position.toLong()
     }
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
-        TODO("Not yet implemented")
+        val textView = TextView(context)
+        textView.text = numberArray[position].toString()
+        textView.textSize = numberArray[position].toFloat()
+        textView.setPadding(5, 10, 0, 10)
+        return textView
     }
 
 }
